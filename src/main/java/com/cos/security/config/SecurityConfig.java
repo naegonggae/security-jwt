2527c7d53f94fe7d8346535aefaf205cd962c46b
naegonggae <tkdtkd97@naver.com>
2023-07-16 22:34:32 +0900

    :sparkles: 시큐리티 회원가입

--- src/main/java/com/cos/security/config/SecurityConfig.java
+++ src/main/java/com/cos/security/config/SecurityConfig.java
@@ -4,13 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+	// 비밀번호 암호화 빈등록
+	@Bean
+	public BCryptPasswordEncoder encodePwd() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -18,10 +23,10 @@ public class SecurityConfig {
 		httpSecurity.csrf().disable();
 		httpSecurity.authorizeHttpRequests()
 				.requestMatchers("/user/**").authenticated()
-				.requestMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN")
+				.requestMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN") // 인증, 권한 둘다 있어야함
 				.requestMatchers("/admin/**").hasRole("ADMIN")
 				.anyRequest().permitAll() // 나머지는 전부 권한없음
-				.and().formLogin().loginPage("/login"); // 권한이 필요한 user, admin, manager 로 가면 login 으로 가도록 설정
+				.and().formLogin().loginPage("/loginForm"); // 권한이 필요한 user, admin, manager 로 가면 loginForm 으로 가도록 설정
 
 		return httpSecurity.build();
 	}
--- src/main/java/com/cos/security/controller/IndexController.java
+++ src/main/java/com/cos/security/controller/IndexController.java
@@ -1,40 +1,63 @@
 package com.cos.security.controller;
 
+import com.cos.security.model.User;
+import com.cos.security.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bc;
 
 	@GetMapping({"", "/"})
 	public String index() {
 		return "index";
 	}
 
-	@GetMapping("/user")
+	@GetMapping("user")
 	public String user() {
 		return "user";
 	}
 
 	@GetMapping("/admin")
-	public @ResponseBody String admin() {
+	public String admin() {
 		return "admin";
 	}
 
 	@GetMapping("/manager")
-	public @ResponseBody String manager() {
+	public String manager() {
 		return "manager";
 	}
 
-	@GetMapping("/login")
-	public @ResponseBody String login() {
-		return "login";
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
 	}
 
-	@GetMapping("/join")
-	public @ResponseBody String join() {
-		return "join";
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "joinForm";
+	}
+
+	@PostMapping("/join")
+	public String join(User user) {
+		System.out.println(user);
+		user.setRole("ROLE_USER");
+
+		// 비밀번호 암호화
+		String rowPassword = user.getPassword();
+		String encPassword = bc.encode(rowPassword);
+		user.setPassword(encPassword);
+
+		userRepository.save(user); // 비밀번호가 노출됨
+		return "redirect:/loginForm";
 	}
 
 	@GetMapping("/joinProc")
